50842947c3fe3ad8f5fa723b0d23cbaa0fe25d5b
ayhan168585 <49242925+ayhan168585@users.noreply.github.com>
2021-10-16 03:30:14 +0300

    16.10.2021-03:30

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
+import { useSelector } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import { useHistory } from "react-router";
+const KEY=process.env.REACT_APP_STRIPE;
 
 const Container = styled.div``;
 
@@ -135,6 +139,29 @@ const Button = styled.button`
 `;
 
 const Cart = () => {
+  const cart = useSelector(state => state.cart)
+  const[stripeToken,setStripeToken]=useState(null)
+  const history=useHistory()
+
+  const onToken=(token)=>{
+    setStripeToken(token)
+  }
+
+  useEffect(()=>{
+    const makeRequest=async()=>{
+      try {
+        const res=await userRequest("/checkout/payment",{
+          tokenId:stripeToken,
+          amount:cart.total *100,
+            
+        });
+        history.push("/success",{data:res.data})
+      } catch (err) {
+        
+      }
+    }
+  },[stripeToken,cart.total,history]);
+  makeRequest();
   return (
     <Container>
       <Announcement />
@@ -151,61 +178,58 @@ const Cart = () => {
         </Top>
         <Bottom>
           <Info>
+           { cart.yogas.map(yoga=>(
             <Yoga>
               <YogaDetail>
-                <Image src="https://res.cloudinary.com/dh70tt9xs/image/upload/v1633654749/vinyasa_yoga_hvcwf9.jpg" />
+                <Image src={yoga.img} />
                 <Details>
                   <YogaName>
-                    <b>Yoga:</b> VİNYASA YOGA
+                    <b>Yoga:</b> {yoga.title}
                   </YogaName>
                   <YogaId>
-                    <b>ID:</b> 615cdc569d4008c9344c2199
+                    <b>ID:</b> {yoga._id}
                   </YogaId>
                   <YogaApplicationType>
-                    <b>Uygulama Tipi:</b>
+                    <b>Uygulama Tipi:</b> {yoga.applicationtype}
                   </YogaApplicationType>
                 </Details>
               </YogaDetail>
               <PriceDetail>
-                <YogaPrice>100,00 TL.</YogaPrice>
+                <YogaPrice>{yoga.price*yoga.quantity},00 TL.</YogaPrice>
               </PriceDetail>
             </Yoga>
+           
+           ))
+}
             <Hr />
-            <Yoga>
-              <YogaDetail>
-                <Image src="https://res.cloudinary.com/dh70tt9xs/image/upload/v1633657120/hatha_yoga_nktfm0.jpg" />
-                <Details>
-                  <YogaName>
-                    <b>Yoga:</b> HATHA YOGA
-                  </YogaName>
-                  <YogaId>
-                    <b>ID:</b> 615cdceabbf9e6ae5647b9da
-                  </YogaId>
-                  <YogaApplicationType>
-                    <b>Uygulama Tipi:</b>
-                  </YogaApplicationType>
-                </Details>
-              </YogaDetail>
-              <PriceDetail>
-                <YogaPrice>150,00 TL.</YogaPrice>
-              </PriceDetail>
-            </Yoga>
+            
           </Info>
           <Summary>
             <SummaryTitle>SİPARİŞ ÖZETİ</SummaryTitle>
             <SummaryItem>
               <SummaryItemText>Ara Toplam</SummaryItemText>
-              <SummaryItemPrice>250,00 TL.</SummaryItemPrice>
+              <SummaryItemPrice>{cart.total},00 TL.</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
               <SummaryItemText>İndirim Miktarı</SummaryItemText>
-              <SummaryItemPrice>-50,00 TL.</SummaryItemPrice>
+              <SummaryItemPrice>-00,00 TL.</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem type="total">
               <SummaryItemText>Ödenecek Toplam</SummaryItemText>
-              <SummaryItemPrice>200,00 TL.</SummaryItemPrice>
+              <SummaryItemPrice>{cart.total},00 TL.</SummaryItemPrice>
             </SummaryItem>
+            <StripeCheckout
+            name="Mürvet'in Yoga Salonu"
+            image="https://avatars.githubusercontent.com/u/1486366?v=4"
+            billingAddress
+            shippingAddress
+            description={`Ödeyeceğiniz Toplam Miktar ${cart.total},00 TL.`}
+            amount={cart.total*100}
+            token={onToken}
+            stripeKey={KEY}
+            >
             <Button>ÖDEME YAP</Button>
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>
